f1dc2e533ed17216030101fc50828af196e433cf
Viki <masalikavika@yandex.ru>
2022-08-27 12:08:09 +0300

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/lnnwt06hh5m77hts?svg=true)](https://ci.appveyor.com/project/Viktorinaaa/post)
---
+++ src/test/java/ru/netology/Post.java
@@ -0,0 +1,40 @@
+package ru.netology;
+
+import io.restassured.mapper.ObjectMapperType;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class Post {
+    @Test
+            void postBodyPass() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("test post") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("test post"));
+    }
+
+    /*@Test
+    void postBodyFail() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("test post") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("test post1"));
+    }*/
+}
